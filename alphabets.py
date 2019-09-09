e9d1106f38a7f5611d1c56fa94e5b130afd89104
sandeepr010 <sandeeprnair1993@gmail.com>
2019-09-09 09:22:16 -0700

    program for print alphabets

---
+++ alphabets.py
@@ -0,0 +1,22 @@
+def lowercaseAlphabets():
+
+    for c in range(97,123):
+        print(chr(c), end = " ");
+
+    print(" ");
+
+def uppercasealphabets():
+
+    for c in range(65,91):
+        print(chr(c), end = " ");
+
+    print(" ");
+
+print("\n");
+print("Uppercase Alphabets \n");
+uppercasealphabets();
+
+print("\n");
+print("Lowercase Alphabets \n");
+lowercaseAlphabets();
+
