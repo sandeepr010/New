3a53e08ae2459511eebe902c196a6f210348fb67
sandeepr010 <sandeeprnair1993@gmail.com>
2019-09-14 13:46:57 -0700

    slice and string length function len()

--- slice.py
+++ slice.py
@@ -1,3 +1,7 @@
 a='Hello, world!'
-print(a[2:5])
+print(a[2:5]) #this will print the string star from positon 2 to position 4 : llo
+
+print(a[-5:-2]) # this start count from back : orl
+
+print(len(a)) # used to know the length of string
 
