4012667c84214824fbc0518a253ec76635cf6239
sandeepr010 <sandeeprnair1993@gmail.com>
2019-09-29 19:31:03 +0530

    Print uppercase and lowercase alphabets

--- alphabets.py
+++ alphabets.py
@@ -1,24 +1,22 @@
 def lowercaseAlphabets():
 
     for c in range(97,123):
-        print(chr(c), end = ",");
+        print(chr(c), end = ",")
 
-    print(" ");
 
 def uppercasealphabets():
 
     for c in range(65,91):
-        print(chr(c), end = ",");
+        print(chr(c), end = ",")
 
-    print(" ");
 
-print("\n");
-print("*"*20,"Uppercase Alphabets","*"*20,"\n");
-uppercasealphabets();
+print("\n")
+print("*"*20,"Uppercase Alphabets","*"*20,"\n")
+uppercasealphabets()
 
-print("\n");
-print("*"*20,"Lowercase Alphabets","*"*20,"\n");
-lowercaseAlphabets();
+print("\n")
+print("*"*20,"Lowercase Alphabets","*"*20,"\n")
+lowercaseAlphabets()
 
 print("\n\n")
 
