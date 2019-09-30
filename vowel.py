33763cf4f2ee8b2856f87a49df75c594d0d22f26
sandeepr010 <sandeeprnair1993@gmail.com>
2019-09-30 20:33:18 +0530

    To find the character is vowel or not

---
+++ vowel.py
@@ -0,0 +1,9 @@
+a = ['a','e','i','o','u']
+b = str(input("\nEnter a charecter : "))
+
+if b in a:
+        print("\n","="*15,"{} is a vowel".format(b),"="*15,"\n")
+
+else : 
+    print("\n","="*15,"{} is not vowel".format(b),"="*15,"\n")
+         
